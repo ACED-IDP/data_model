7437f171207fb9c9deafa5cceaf18700075eea94
Brian Walsh <brian@bwalsh.com>
2022-10-11 18:21:51 -0700

    Adds symmetric_difference

--- scripts/model_analysis.py
+++ scripts/model_analysis.py
@@ -28,13 +28,13 @@ for file_name in output_dir.glob("*.ndjson"):
                 models[entity_name][entity_keys] = 0
             models[entity_name][entity_keys] += 1
 print("######")
-print("Distinct fields per entity")
+print("Distinct fieldsets per entity")
 print("######")
 for entity_name in sorted(models):
     print(entity_name, len(models[entity_name]))
 
 print("######")
-print("Detail fields per entity")
+print("Detail fieldsets per entity")
 print("######")
 for entity_name in sorted(models):
     print(entity_name)
@@ -42,4 +42,27 @@ for entity_name in sorted(models):
     for key_set in sorted_keys_by_frequency:
         print('\t', models[entity_name][key_set], key_set)
 
-# pprint(models)
+
+print("######")
+print("Detail fieldsets per entity")
+print("######")
+for entity_name in sorted(models):
+    print(entity_name)
+    sorted_keys_by_frequency = dict(sorted(models[entity_name].items(), key=lambda item: item[1]))
+    for key_set in sorted_keys_by_frequency:
+        print('\t', models[entity_name][key_set], key_set)
+
+
+print("######")
+print("Diff - top two field sets for each entity")
+print("######")
+for entity_name in sorted(models):
+    sorted_keys_by_frequency = dict(sorted(models[entity_name].items(), key=lambda item: item[1]))
+    if len(sorted_keys_by_frequency) == 1:
+        continue
+    print(entity_name)
+    most_popular = set(list(sorted_keys_by_frequency.keys())[-1].split(','))
+    second_most_popular = set(list(sorted_keys_by_frequency.keys())[-2].split(','))
+    print('\t symmetric_difference', most_popular.symmetric_difference(second_most_popular))
+    print('\t\t fields in the second most popular not in most popular', second_most_popular - most_popular)
+    print('\t\t fields in the most popular not in second most popular', most_popular - second_most_popular)
