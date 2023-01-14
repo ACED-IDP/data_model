367d91569c422e376364b63211a4ea4c2f3d01e3
Brian Walsh <brian@bwalsh.com>
2023-01-13 18:03:25 -0800

    FHIR service

---
+++ scripts/truncate_hapi_fhir_tables.sql
@@ -0,0 +1,9 @@
+\c  hapi;
+
+DO $$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE ';
+    END LOOP;
+END $$;
